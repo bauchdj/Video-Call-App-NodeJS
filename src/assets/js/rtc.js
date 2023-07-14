f404e5e7de69c49218754079515e917b03dcc15b
bauchdj <jamesbauch@gmail.com>
2023-07-14 02:41:04 -0600

    Updated rtc after HEAVY modification. Added ice-server.json support

--- src/assets/js/rtc.js
+++ src/assets/js/rtc.js
@@ -3,10 +3,12 @@
  * @date 6th January, 2020
  */
 import h from './helpers.js';
+import iceServer from '../json/ice-server.json' assert { type: 'json' };
 
 window.addEventListener( 'load', () => {
     const room = h.getQString( location.href, 'room' );
     const username = sessionStorage.getItem( 'username' );
+	document.querySelector('#video-username').children[0].title = username;
 
     if ( !room ) {
         document.querySelector( '#room-create' ).attributes.removeNamedItem( 'hidden' );
@@ -17,6 +19,7 @@ window.addEventListener( 'load', () => {
     }
 
     else {
+		document.getElementById('navbar').style.visibility = 'unset';
         let commElem = document.getElementsByClassName( 'room-comm' );
 
         for ( let i = 0; i < commElem.length; i++ ) {
@@ -24,6 +27,7 @@ window.addEventListener( 'load', () => {
         }
 
         var pc = [];
+		var peerNamesById = {};
 
         let socket = io( '/stream' );
 
@@ -33,6 +37,7 @@ window.addEventListener( 'load', () => {
         var screen = '';
         var recordedStream = [];
         var mediaRecorder = '';
+		var screenSharingBool = false;
 
         //Get user video by default
         getAndSetUserStream();
@@ -41,7 +46,7 @@ window.addEventListener( 'load', () => {
         socket.on( 'connect', () => {
             //set socketId
             socketId = socket.io.engine.id;
-            document.getElementById('randomNumber').innerText = randomNumber;
+            //document.getElementById('randomNumber').innerText = randomNumber;
 
 
             socket.emit( 'subscribe', {
@@ -51,21 +56,22 @@ window.addEventListener( 'load', () => {
 
 
             socket.on( 'new user', ( data ) => {
-                socket.emit( 'newUserStart', { to: data.socketId, sender: socketId } );
+                socket.emit( 'newUserStart', { to: data.socketId, sender: { id: socketId, name: username }});
                 pc.push( data.socketId );
                 init( true, data.socketId );
             } );
 
-
             socket.on( 'newUserStart', ( data ) => {
-                pc.push( data.sender );
-                init( false, data.sender );
+				const senderId = data.sender.id;
+				peerNamesById[senderId] = data.sender.name;
+                pc.push( senderId );
+                init( false, senderId );
             } );
 
 
             socket.on( 'ice candidates', async ( data ) => {
                 data.candidate ? await pc[data.sender].addIceCandidate( new RTCIceCandidate( data.candidate ) ) : '';
-            } );
+           	});
 
 
             socket.on( 'sdp', async ( data ) => {
@@ -73,8 +79,9 @@ window.addEventListener( 'load', () => {
                     data.description ? await pc[data.sender].setRemoteDescription( new RTCSessionDescription( data.description ) ) : '';
 
                     h.getUserFullMedia().then( async ( stream ) => {
-                        if ( !document.getElementById( 'local' ).srcObject ) {
-                            h.setLocalStream( stream );
+                        if ( !document.getElementById('local').srcObject ) {
+							//console.log('on socket offer setLocalStream');
+							h.setLocalStream( stream );
                         }
 
                         //save my stream
@@ -85,17 +92,17 @@ window.addEventListener( 'load', () => {
                         } );
 
                         let answer = await pc[data.sender].createAnswer();
-
                         await pc[data.sender].setLocalDescription( answer );
-
-                        socket.emit( 'sdp', { description: pc[data.sender].localDescription, to: data.sender, sender: socketId } );
+                        socket.emit( 'sdp', { description: pc[data.sender].localDescription, to: data.sender, sender: { id: socketId, name: username }});
                     } ).catch( ( e ) => {
                         console.error( e );
                     } );
                 }
 
                 else if ( data.description.type === 'answer' ) {
-                    await pc[data.sender].setRemoteDescription( new RTCSessionDescription( data.description ) );
+					const senderId = data.sender.id;
+					peerNamesById[senderId] = data.sender.name;
+                    await pc[senderId].setRemoteDescription( new RTCSessionDescription( data.description ) );
                 }
             } );
 
@@ -108,6 +115,7 @@ window.addEventListener( 'load', () => {
 
         function getAndSetUserStream() {
             h.getUserFullMedia().then( ( stream ) => {
+//console.log('getAndSetUserStream fn');
                 //save my stream
                 myStream = stream;
 
@@ -135,21 +143,25 @@ window.addEventListener( 'load', () => {
 
 
         function init( createOffer, partnerName ) {
-            pc[partnerName] = new RTCPeerConnection( h.getIceServer() );
+            pc[partnerName] = new RTCPeerConnection(iceServer);
 
             if ( screen && screen.getTracks().length ) {
+//console.log('screen:', screen);
                 screen.getTracks().forEach( ( track ) => {
                     pc[partnerName].addTrack( track, screen );//should trigger negotiationneeded event
                 } );
+				updateAudioTrack(); // if screen sharing when someone joins updateAudio based on mic state
             }
 
             else if ( myStream ) {
+//console.log('myStream:', myStream);
                 myStream.getTracks().forEach( ( track ) => {
                     pc[partnerName].addTrack( track, myStream );//should trigger negotiationneeded event
                 } );
             }
 
             else {
+//console.log('get my stream please');
                 h.getUserFullMedia().then( ( stream ) => {
                     //save my stream
                     myStream = stream;
@@ -158,21 +170,33 @@ window.addEventListener( 'load', () => {
                         pc[partnerName].addTrack( track, stream );//should trigger negotiationneeded event
                     } );
 
-                    h.setLocalStream( stream );
+	                h.setLocalStream(stream);
+/*
+					setTimeout(() => {
+document.querySelector('#local').play() 
+	.then(() => {
+		console.log('Playback started');
+	})
+	.catch(error => {
+		console.error('Playback failed:', error);
+}); 
+						console.log('set timeout for broadcast');
+	                	broadcastNewTracks(myStream, 'video');
+	                	broadcastNewTracks(myStream, 'audio');
+						document.getElementById('local').play();
+					}, 1000);
+*/
                 } ).catch( ( e ) => {
                     console.error( `stream error: ${ e }` );
                 } );
             }
 
 
-
             //create offer
             if ( createOffer ) {
                 pc[partnerName].onnegotiationneeded = async () => {
                     let offer = await pc[partnerName].createOffer();
-
                     await pc[partnerName].setLocalDescription( offer );
-
                     socket.emit( 'sdp', { description: pc[partnerName].localDescription, to: partnerName, sender: socketId } );
                 };
             }
@@ -189,8 +213,13 @@ window.addEventListener( 'load', () => {
             //add
             pc[partnerName].ontrack = ( e ) => {
                 let str = e.streams[0];
-                if ( document.getElementById( `${ partnerName }-video` ) ) {
-                    document.getElementById( `${ partnerName }-video` ).srcObject = str;
+				const remoteVideo = document.getElementById(`${ partnerName }-video`);
+                if (remoteVideo) {
+                    remoteVideo.srcObject = str;
+					remoteVideo.addEventListener('loadedmetadata', e => {
+						e.target.play();
+						h.resizeObserver.observe(e.target.parentNode);
+					});
                 }
 
                 else {
@@ -200,6 +229,10 @@ window.addEventListener( 'load', () => {
                     newVid.srcObject = str;
                     newVid.autoplay = true;
                     newVid.className = 'remote-video';
+					newVid.setAttribute('playsinline', '');
+					newVid.setAttribute('webkit-playsinline', '');
+					//if (h.iosDevice) { newVid.classList.add('ios-video') };
+					h.iosDeviceAddOns(newVid);
 
                     //video controls elements
                     let controlDiv = document.createElement( 'div' );
@@ -207,17 +240,30 @@ window.addEventListener( 'load', () => {
                     controlDiv.innerHTML = `<i class="fa fa-microphone text-white pr-3 mute-remote-mic" title="Mute"></i>
                         <i class="fa fa-expand text-white expand-remote-video" title="Expand"></i>`;
 
+					const nameDiv = document.createElement('div');
+					nameDiv.id = 'video-username';
+					nameDiv.appendChild(document.createElement('p'));
+					nameDiv.childNodes[0].style.margin = '0';
+					//nameDiv.childNodes[0].textContent = (Math.random() < 0.5) ? "Good looking" : "Need a brush up";
+					nameDiv.childNodes[0].textContent = peerNamesById[partnerName];
+
+					const handle = document.createElement('div');
+					handle.classList.add('my-handle');
+					handle.setAttribute('title', 'Drag and swap handle');
+
                     //create a new div for card
                     let cardDiv = document.createElement( 'div' );
                     cardDiv.className = 'card card-sm';
                     cardDiv.id = partnerName;
                     cardDiv.appendChild( newVid );
                     cardDiv.appendChild( controlDiv );
+                    cardDiv.appendChild(nameDiv);
+                    cardDiv.appendChild(handle);
 
                     //put div in main-section elem
                     document.getElementById( 'videos' ).appendChild( cardDiv );
 
-                    h.adjustVideoElemSize();
+                    //h.adjustVideoElemSize();
                 }
             };
 
@@ -260,9 +306,11 @@ window.addEventListener( 'load', () => {
 
                 //save my screen stream
                 screen = stream;
+				screenSharingBool = true;
 
                 //share the new stream with all partners
                 broadcastNewTracks( stream, 'video', false );
+				updateAudioTrack();
 
                 //When the stop sharing button shown by the browser is clicked
                 screen.getVideoTracks()[0].addEventListener( 'ended', () => {
@@ -273,11 +321,10 @@ window.addEventListener( 'load', () => {
             } );
         }
 
-
-
         function stopSharingScreen() {
             //enable video toggle btn
             h.toggleVideoBtnDisabled( false );
+			screenSharingBool = false;
 
             return new Promise( ( res, rej ) => {
                 screen.getTracks().length ? screen.getTracks().forEach( track => track.stop() ) : '';
@@ -285,25 +332,34 @@ window.addEventListener( 'load', () => {
                 res();
             } ).then( () => {
                 h.toggleShareIcons( false );
+				updateAudioTrack();
                 broadcastNewTracks( myStream, 'video' );
             } ).catch( ( e ) => {
                 console.error( e );
             } );
         }
 
-
-
         function broadcastNewTracks( stream, type, mirrorMode = true ) {
+			//console.log('broadcast set local stream');
             h.setLocalStream( stream, mirrorMode );
 
             let track = type == 'audio' ? stream.getAudioTracks()[0] : stream.getVideoTracks()[0];
 
-            for ( let p in pc ) {
-                let pName = pc[p];
+			for (let i = 0; i < pc.length; i++) {
+                const partnerName = pc[i];
+				const recipientPeer = pc[partnerName];
 
-                if ( typeof pc[pName] == 'object' ) {
-                    h.replaceTrack( track, pc[pName] );
-                }
+				if (recipientPeer.getSenders) { 
+    				const rtcSender = recipientPeer.getSenders().find(s => {
+						// console.log("track.kind:", track.kind);
+        				return s.track && s.track.kind === track.kind;
+					});
+					// console.log('recipientPeer:', recipientPeer);
+					// console.log("rtcSender:", rtcSender);
+       				rtcSender.replaceTrack( track );
+				} else {
+					return; 
+				}
             }
         }
 
@@ -313,14 +369,12 @@ window.addEventListener( 'load', () => {
 
             if ( isRecording ) {
                 e.setAttribute( 'title', 'Stop recording' );
-                e.children[0].classList.add( 'text-danger' );
-                e.children[0].classList.remove( 'text-white' );
+                e.classList.replace('text-white', 'text-danger');
             }
 
             else {
                 e.setAttribute( 'title', 'Record' );
-                e.children[0].classList.add( 'text-white' );
-                e.children[0].classList.remove( 'text-danger' );
+                e.classList.replace('text-danger', 'text-white');
             }
         }
 
@@ -376,57 +430,92 @@ window.addEventListener( 'load', () => {
             }
         } );
 
-
-        //When the video icon is clicked
-        document.getElementById( 'toggle-video' ).addEventListener( 'click', ( e ) => {
-            e.preventDefault();
-
-            let elem = document.getElementById( 'toggle-video' );
-
+	const toggleMyVideo = () => {
+            let elem = document.getElementById('toggle-video');
             if ( myStream.getVideoTracks()[0].enabled ) {
-                e.target.classList.remove( 'fa-video' );
-                e.target.classList.add( 'fa-video-slash' );
+                elem.classList.replace('fa-video', 'fa-video-slash');
                 elem.setAttribute( 'title', 'Show Video' );
-
                 myStream.getVideoTracks()[0].enabled = false;
-            }
-
-            else {
-                e.target.classList.remove( 'fa-video-slash' );
-                e.target.classList.add( 'fa-video' );
+            } else {
+                elem.classList.replace('fa-video-slash', 'fa-video');
                 elem.setAttribute( 'title', 'Hide Video' );
-
                 myStream.getVideoTracks()[0].enabled = true;
             }
-
             broadcastNewTracks( myStream, 'video' );
-        } );
+	}
 
+        document.addEventListener('keydown', e => { if (e.key === 'c') { toggleMyVideo() } });
+
+        //When the video icon is clicked
+        document.getElementById('toggle-video').addEventListener( 'click', ( e ) => {
+        	e.preventDefault();
+			toggleMyVideo();
+        });
+
+        //only on mobile devices
+        const switchCamera = () => {
+			navigator.mediaDevices.enumerateDevices()
+				.then(devices => {
+					const videoDevices = devices.filter(device => {
+						const label = device.label.toLowerCase();
+  						return device.kind === 'videoinput' && label.includes('back');
+					});
+					const deviceConstraint = { deviceId: videoDevices.at(-1).deviceId }; //the constraint object determines what media is allowed, in this case video
+					const whichCamera = myStream.getVideoTracks()[0].getSettings().facingMode == "environment" ? "user" : deviceConstraint; //toggles the camera between front and rear
+					h.getUserFullMedia(whichCamera).then(stream => {
+						myStream = stream;
+						const mirrorMode = myStream.getVideoTracks()[0].getSettings().facingMode == "user" ? true : false;
+						broadcastNewTracks(myStream, 'video', mirrorMode);
+					});
+				})
+				.catch(error => {
+					console.error('Error enumerating devices:', error);
+				});
+        }
+        document.getElementById('flip-video').addEventListener( 'click', ( e ) => {
+        	e.preventDefault();
+            switchCamera();
+        });
+
+	function updateAudioTrack() {
+		const micAudioTrack = myStream.getAudioTracks()[0];
+
+		const updateAttrs = (title) => {
+			const elem = document.getElementById('toggle-mute');
+			elem.classList.toggle('fa-microphone-alt', micAudioTrack.enabled);
+			elem.classList.toggle('fa-microphone-alt-slash', !micAudioTrack.enabled);
+			elem.setAttribute('title', title);
+		};
+
+		if (screenSharingBool) {
+			// console.log('sharing screen audio: updateing mic icon and audio track');
+			if (micAudioTrack.enabled) {
+				updateAttrs('Share screen audio');
+				broadcastNewTracks(myStream, 'audio');
+				broadcastNewTracks(screen, 'video', false);
+			} else {
+				updateAttrs('Currently sharing screen audio');
+				broadcastNewTracks(screen, 'audio', false);
+			}
+		} else {
+			updateAttrs(micAudioTrack.enabled ? 'Mute' : 'Unmute');
+			broadcastNewTracks(myStream, 'audio');
+		}
+	}
+
+		const toggleAudio = () => {
+			const micAudioTrack = myStream.getAudioTracks()[0];
+			micAudioTrack.enabled = !micAudioTrack.enabled; // toggle mic audio
+			updateAudioTrack();
+		}
+
+        document.addEventListener('keydown', e => { if (e.key === 'm') { toggleAudio() } });
 
         //When the mute icon is clicked
-        document.getElementById( 'toggle-mute' ).addEventListener( 'click', ( e ) => {
+        document.getElementById('toggle-mute').addEventListener( 'click', ( e ) => {
             e.preventDefault();
-
-            let elem = document.getElementById( 'toggle-mute' );
-
-            if ( myStream.getAudioTracks()[0].enabled ) {
-                e.target.classList.remove( 'fa-microphone-alt' );
-                e.target.classList.add( 'fa-microphone-alt-slash' );
-                elem.setAttribute( 'title', 'Unmute' );
-
-                myStream.getAudioTracks()[0].enabled = false;
-            }
-
-            else {
-                e.target.classList.remove( 'fa-microphone-alt-slash' );
-                e.target.classList.add( 'fa-microphone-alt' );
-                elem.setAttribute( 'title', 'Mute' );
-
-                myStream.getAudioTracks()[0].enabled = true;
-            }
-
-            broadcastNewTracks( myStream, 'audio' );
-        } );
+			toggleAudio();
+        });
 
 
         //When user clicks the 'Share screen' button
